b1a3edbcfc4ef22f85dfd34e41172c5bad676061
glanderson42 <glanderson@protonmail.com>
2019-12-08 21:58:41 +0100

    fix formating

--- tuple.hpp
+++ tuple.hpp
@@ -5,45 +5,46 @@
 
 namespace tpl {
 
-	template<class First, class... Rest>
-	struct tuple : public tuple<Rest...> {
-		First first;
+template<class First, class... Rest>
+struct tuple : public tuple<Rest...> {
+	First first;
 		
-		tuple(const First first, const Rest... rest) noexcept
-			: tuple<Rest...>(rest...)
-			, first(first)
-		{}
-	};
+	tuple(const First first, const Rest... rest) noexcept
+		: tuple<Rest...>(rest...)
+		, first(first)
+	{}
+};
 
-	template<class First>
-	struct tuple<First> {
-		First first;
+template<class First>
+struct tuple<First> {
+	First first;
 
-		tuple(const First first) noexcept
-			: first(first)
-		{}
-	};
+	tuple(const First first) noexcept
+		: first(first)
+	{}
+};
 
-	template<std::size_t Index, class First, class... Rest>
-	struct impl {
-		static auto value(const tuple<First, Rest...>* tpl) 
-			-> decltype(impl<Index - 1, Rest...>::value(tpl)) const {
-			return impl<Index - 1, Rest...>::value(tpl);
-		}
-	};
-
-	template<class First, class... Rest>
-	struct impl<0, First, Rest...> {
-		static First value(const tuple<First, Rest...>* tpl) {
-			return tpl->first;
-		}
-	};
-
-	template<std::size_t Index, class First, class... Rest>
-	auto get(const tuple<First, Rest...>& tpl)
-		-> decltype(impl<Index, First, Rest...>::value(&tpl)) const {
-		return impl<Index, First, Rest...>::value(&tpl);
+template<std::size_t Index, class First, class... Rest>
+struct impl {
+	static auto value(const tuple<First, Rest...>* tpl) 
+		-> decltype(impl<Index - 1, Rest...>::value(tpl)) const {
+		return impl<Index - 1, Rest...>::value(tpl);
 	}
+};
+
+template<class First, class... Rest>
+struct impl<0, First, Rest...> {
+	static First value(const tuple<First, Rest...>* tpl) {
+		return tpl->first;
+	}
+};
+
+template<std::size_t Index, class First, class... Rest>
+auto get(const tuple<First, Rest...>& tpl)
+	-> decltype(impl<Index, First, Rest...>::value(&tpl)) const {
+	return impl<Index, First, Rest...>::value(&tpl);
 }
+
+} // tpl
 
 #endif // TUPLE_HPP
